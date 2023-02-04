f893d5ce20e6b145d42705532f12704875cff1b5
neelbamroliya <neelbamroliya0@gmail.com>
2023-02-04 11:11:07 +0530

    added dotenv file for secure app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express"
 import mongoose from "mongoose"
+import { config } from "dotenv"
 
 import Deck from "./models/deck"
 
+config()
 const app = express()
 app.use(express.json())
-const PORT = 5000
-
 
 app.post("/decks", async (req: Request, res: Response) => {
   console.log(req.body);
@@ -20,9 +20,9 @@ app.post("/decks", async (req: Request, res: Response) => {
 })
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://owner:coV7FZLhCTHcwj1n@cluster0.3b27rx8.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URL!)
   .then(() => {
     console.log("database connected successfully")
-    app.listen(PORT)
+    app.listen(process.env.PORT)
   })
   .catch((err) => console.log(`error : ${err.massage}`))
